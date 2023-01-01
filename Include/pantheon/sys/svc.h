38d418dadff3402c43236c7e9c731779d4e85cee
Brian Schnepp <bschneppdev@gmail.com>
2022-12-31 18:50:05 -0700

    Add additional documentation

--- Include/pantheon/sys/svc.h
+++ Include/pantheon/sys/svc.h
@@ -10,9 +10,24 @@
 #include "../types.h"
 
 Result SvcExitProcess();
+
 Result SvcLogText(const char *Content);
+
 Result *SvcAllocateBuffer(uint64_t Sz);
-bool SvcCreateThread(void (*Entry)(void*), void *Reserved, void *StackTop, uint8_t Priority);
+
+/**
+ * \~english @brief Creates a thread for this process
+ * \~english @param[in] Entry The entrypoint of the new thread
+ * \~english @param[in] Argument The argument to provide to the entrypoint on the new thread
+ * \~english @param[in] StackTop The location of the stack of the new thread
+ * \~english @param[in] Priority The priority of the new thread
+ * \~english @pre
+ * 	- StackTop must be a valid address within the process' address space
+ * 	- Priority must be between 0 and 5, with higher priority being higher values
+ * \~english @post
+ * 	- The thread will be placed in a Running state, and possibly have started execution
+ */
+Result SvcCreateThread(void (*Entry)(void*), void *Argument, void *StackTop, uint8_t Priority);
 
 Result SvcCreateNamedEvent(const char *Name, int32_t *Read, int32_t *Write);
 Result SvcSignalEvent(int32_t Handle);
@@ -21,6 +36,19 @@ Result SvcResetEvent(int32_t Handle);
 bool SvcPollEvent(int32_t Handle);
 Result SvcYield();
 Result SvcExecute(uint8_t Handle);
+
+/**
+ * \~english @brief Creates a Port for this process
+ * \~english @param[in] Name The 8-character name for this Port, if applicable.
+ * \~english @param[in] MaxConnections The maximum number of connections for this Port
+ * \~english @param[in] HandleServer The location to store the handle for ServerConnections
+ * \~english @param[in] HandleClient The location to store the handle for ClientConnections
+ * \~english @pre
+ * 	- Ports will be anonymous (no name) if no 8-character name is provided.
+ * 	- Permission to create named ports is restricted: if not allowed, the returned Result will reflect this
+ * 	- Provided names must be 8 characters: exceeding this will result in truncation.
+ * 	- Argument Name must be null-terminated, but the null character does not apply to the 8 character limit.
+ */
 Result SvcCreatePort(const char *Name, int64_t MaxConnections, int32_t *HandleServer, int32_t *HandleClient);
 Result SvcConnectToPort(int32_t *ClientPortHandle, int32_t *OutClientConnectionHandle);
 Result SvcConnectToNamedPort(const char *Name, int32_t *OutClientConnectionHandle);
--- Source/sys/aarch64/tls.c
+++ Source/sys/aarch64/tls.c
@@ -8,6 +8,23 @@
 #include "pantheon/types.h"
 #include "pantheon/sys/tls.h"
 
+/**
+ * \~english @brief Obtains the thread-local storage area
+ * \~english @details Obtains the thread-local storage area, which is needed
+ * to perform certain operations such as IPC. The location of this area is
+ * determined by the kernel, and may not be changed. However, it's contents
+ * are writable by any thread in the current process: it is up to the
+ * user to enforce that only the thread which the thread-local storage is
+ * intended for can actually write to it's own thread-local storage.
+ * <br>
+ * Typically, IPC will require that some messages be parsed and "blessed" by
+ * the kernel before proceeding: this is required for operations such as
+ * transferring of handles and association with typical pantheon system 
+ * processes.
+ * 
+ * \~english @return A void* pointing to where the thread-local storage is.
+ * @see pantheon::ipc
+ */
 void *GetTLS(void)
 {
 	void *RetVal = 0;
